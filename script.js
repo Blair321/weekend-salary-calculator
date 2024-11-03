0123329a0d1637410d19b139f39c0dec21375b22
Blair Jilek <blairjilek@gmail.com>
2024-11-03 11:18:22 -0600

    created function for first name to get a better undertsanding

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// on ready function
 // declare my inputs at least first name first to make sure I'm doing it right. 
-const firstNameInput = document.querySelector('#firstNameInput');
+function onReady() {
+    console.log('It is ready time to code!'); 
+}
+  onReady();
 
+const firstName = document.querySelector('#firstName');
+//create function to add first name on click
 function getFirstname() {
+    console.log('First employee name', firstName);
+    firstName = document.querySelector('#firstName')
+    firstName.innerHTML += '<button onClick="submitButton(event)"></Button>'
     
 }
