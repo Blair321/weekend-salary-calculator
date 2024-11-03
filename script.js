85231f6efb6d6a39b25ea2754337f7afd79683f1
Blair Jilek <blairjilek@gmail.com>
2024-11-03 13:26:23 -0600

    declared all inputs by id and matched from html

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // on ready function
-// declare my inputs at least first name first to make sure I'm doing it right. 
+// declare my inputs at least first name first to make sure I'm doing it right.
+// declare other inputs 
 function onReady() {
     console.log('It is ready time to code!'); 
 }
   onReady();
 
-const firstName = document.querySelector('#firstName');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const Id = document.getElementById('Number');
+const jobTitle = document.getElementById('jobTitle');
+const salary = document.getElementById('Income')
 //create function to add first name on click
 // We actually want to create a function that add all the values inside the table 
-
+function addNewEmployee() {
+  
+}
 
 
     
