ad14d76626bf0c4bb522bd8e52bdaa060c70071a
Blair Jilek <blairjilek@gmail.com>
2024-11-03 10:35:19 -0600

    declared my first input in js and added onclick for my submit button

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// declare my inputs at least first name first to make sure I'm doing it right. 
+const firstNameInput = document.querySelector('#firstNameInput');
 
+function getFirstname() {
+    
+}
